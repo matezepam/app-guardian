a70a9f5c74296b3899d73aafe68c3570f6e0bb7a
Mateo. <mateosalazarespinoza@hotmail.com>
2026-01-12 11:04:59 -0500

    Schema Users

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,11 +4,13 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  firstName: String,
-  lastName: String,
-  phone: String,
-  country: String,
-  createdAt: { type: Date, default: Date.now },
-});
+  firstName: { type: String },
+  lastName: { type: String },
+  phone: { type: String },
+  country: { type: String },
+  avatar: { type: String },
+  bio: { type: String, default: '' },
+  socials: { type: Object, default: {} }
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
